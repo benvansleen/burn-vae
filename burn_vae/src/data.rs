74d7a9d8474c6ef17f4616746eb00182650259a2
Ben Van Sleen <78059325+benvansleen@users.noreply.github.com>
2024-01-15 13:06:33 -0600

    condition on radius -- recon loss of 0.17 (#3)

---
+++ burn_vae/src/data.rs
@@ -0,0 +1,140 @@
+use burn::{
+    data::{dataloader::batcher::Batcher, dataset::Dataset},
+    tensor::{backend::Backend, Tensor},
+};
+use flume::{bounded, Receiver, Sender};
+use once_cell::sync::OnceCell;
+use pyo3::{prelude::*, types::IntoPyDict};
+
+pub const INPUT_DIM: usize = 3;
+pub const LABEL_DIM: usize = 1;
+type Point = [f32; INPUT_DIM];
+pub type PointTensor<B> = Tensor<B, 3>;
+pub type LatentTensor<B> = Tensor<B, 3>;
+
+static CHAN: OnceCell<(
+    Sender<SpiralItem>,
+    Receiver<SpiralItem>,
+)> = OnceCell::new();
+static T: OnceCell<[std::thread::JoinHandle<()>; 2]> =
+    OnceCell::new();
+
+#[derive(Debug, Clone)]
+pub struct SpiralItem {
+    pub point: Point,
+    pub label: f32,
+}
+
+#[derive(Debug, Clone)]
+pub struct SpiralDataset {
+    pub epoch_size: usize,
+    ch: Receiver<SpiralItem>,
+}
+
+fn generate_data(n_samples: u32, tx: Sender<SpiralItem>) {
+    Python::with_gil(|py| {
+        let ds = py.import("sklearn.datasets").unwrap();
+        let swiss = ds.getattr("make_swiss_roll").unwrap();
+        let kwargs = [("n_samples", n_samples)].into_py_dict(py);
+
+        loop {
+            let (points, ts): (Vec<[f32; 3]>, Vec<f32>) = swiss
+                .call((), Some(kwargs))
+                .unwrap()
+                .extract()
+                .unwrap();
+
+            points.into_iter().zip(ts).for_each(|(pt, t)| {
+                tx.send(SpiralItem {
+                    point: pt,
+                    label: t,
+                })
+                .expect("failed to send item to channel");
+            });
+        }
+    })
+}
+
+impl SpiralDataset {
+    pub fn new(epoch_size: usize) -> Self {
+        let (tx, rx) = CHAN.get_or_init(|| bounded(10_000_000));
+        T.get_or_init(|| {
+            std::array::from_fn(|_| {
+                let tx = tx.clone();
+                std::thread::spawn(move || {
+                    generate_data(1000, tx);
+                })
+            })
+        });
+
+        Self {
+            epoch_size,
+            ch: rx.clone(),
+        }
+    }
+}
+
+impl Dataset<SpiralItem> for SpiralDataset {
+    fn get(&self, _idx: usize) -> Option<SpiralItem> {
+        self.ch.recv().ok()
+    }
+
+    fn len(&self) -> usize {
+        self.epoch_size
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SpiralBatcher<B: Backend> {
+    device: B::Device,
+}
+
+impl<B: Backend> SpiralBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SpiralBatch<B: Backend> {
+    pub points: PointTensor<B>,
+    pub labels: Tensor<B, 2>,
+}
+
+impl<B: Backend> Batcher<SpiralItem, SpiralBatch<B>>
+    for SpiralBatcher<B>
+{
+    fn batch(&self, items: Vec<SpiralItem>) -> SpiralBatch<B> {
+        let batch = items
+            .iter()
+            .map(|item| {
+                (
+                    Tensor::<B, 1>::from_floats(item.point),
+                    Tensor::<B, 1>::from_floats([item.label]),
+                )
+            })
+            .map(|(p, l)| {
+                let (p_dim, l_dim) = (p.dims()[0], l.dims()[0]);
+                (p.reshape([1, 1, p_dim]), l.reshape([1, l_dim]))
+            });
+
+        let points = Tensor::cat(
+            batch.clone().map(|(p, _)| p).collect(),
+            0,
+        )
+        .to_device(&self.device);
+        let labels =
+            Tensor::cat(batch.map(|(_, l)| l).collect(), 0)
+                .to_device(&self.device);
+
+        SpiralBatch { points, labels }
+    }
+}
+
+pub fn get_data(n: usize) -> (Vec<Vec<f32>>, Vec<f32>) {
+    let dataset = SpiralDataset::new(n);
+    (0..n)
+        .map(|i| dataset.get(i).unwrap())
+        .map(|item| (item.point.to_vec(), item.label))
+        .unzip()
+}
---
+++ burn_vae/src/metric.rs
@@ -0,0 +1,209 @@
+use burn::{
+    tensor::{backend::Backend, ElementConversion, Tensor},
+    train::metric::{
+        state::{FormatOptions, NumericMetricState},
+        Adaptor, LossInput, Metric, MetricEntry, MetricMetadata,
+        Numeric,
+    },
+};
+
+pub struct VAEOutput<B: Backend> {
+    pub recon_loss: Tensor<B, 1>,
+    pub kl_loss: Tensor<B, 1>,
+}
+
+impl<B: Backend> VAEOutput<B> {
+    pub fn new(
+        recon_loss: Tensor<B, 1>,
+        kl_loss: Tensor<B, 1>,
+    ) -> Self {
+        Self {
+            recon_loss,
+            kl_loss,
+        }
+    }
+}
+
+impl<B: Backend> Adaptor<LossInput<B>> for VAEOutput<B> {
+    fn adapt(&self) -> LossInput<B> {
+        LossInput::new(
+            self.kl_loss.clone() + self.recon_loss.clone(),
+        )
+    }
+}
+
+impl<B: Backend> Adaptor<ReconstructionLossInput<B>>
+    for VAEOutput<B>
+{
+    fn adapt(&self) -> ReconstructionLossInput<B> {
+        ReconstructionLossInput::new(self.recon_loss.clone())
+    }
+}
+
+impl<B: Backend> Adaptor<KLLossInput<B>> for VAEOutput<B> {
+    fn adapt(&self) -> KLLossInput<B> {
+        KLLossInput::new(self.kl_loss.clone())
+    }
+}
+
+impl<B: Backend> Adaptor<()> for VAEOutput<B> {
+    fn adapt(&self) {}
+}
+
+pub struct ReconstructionLossInput<B: Backend> {
+    tensor: Tensor<B, 1>,
+}
+
+impl<B: Backend> ReconstructionLossInput<B> {
+    pub fn new(tensor: Tensor<B, 1>) -> Self {
+        Self { tensor }
+    }
+}
+
+#[derive(Default)]
+pub struct ReconstructionLossMetric<B: Backend> {
+    state: NumericMetricState,
+    _b: B,
+}
+
+impl<B: Backend> ReconstructionLossMetric<B> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl<B: Backend> Metric for ReconstructionLossMetric<B> {
+    type Input = ReconstructionLossInput<B>;
+    const NAME: &'static str = "Reconstruction Loss";
+
+    fn update(
+        &mut self,
+        loss: &Self::Input,
+        _metadata: &MetricMetadata,
+    ) -> MetricEntry {
+        let loss = f64::from_elem(
+            loss.tensor.clone().mean().into_data().value[0],
+        );
+        self.state.update(
+            loss,
+            1,
+            FormatOptions::new(Self::NAME).precision(2),
+        )
+    }
+
+    fn clear(&mut self) {
+        self.state.reset();
+    }
+}
+
+impl<B: Backend> Numeric for ReconstructionLossMetric<B> {
+    fn value(&self) -> f64 {
+        self.state.value()
+    }
+}
+
+pub struct KLLossInput<B: Backend> {
+    tensor: Tensor<B, 1>,
+}
+
+impl<B: Backend> KLLossInput<B> {
+    pub fn new(tensor: Tensor<B, 1>) -> Self {
+        Self { tensor }
+    }
+}
+
+#[derive(Default)]
+pub struct KLLossMetric<B: Backend> {
+    state: NumericMetricState,
+    _b: B,
+}
+
+impl<B: Backend> KLLossMetric<B> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl<B: Backend> Metric for KLLossMetric<B> {
+    type Input = KLLossInput<B>;
+    const NAME: &'static str = "KL Loss";
+
+    fn update(
+        &mut self,
+        loss: &Self::Input,
+        _metadata: &MetricMetadata,
+    ) -> MetricEntry {
+        let loss = f64::from_elem(
+            loss.tensor.clone().mean().into_data().value[0],
+        );
+        self.state.update(
+            loss,
+            1,
+            FormatOptions::new(Self::NAME).precision(2),
+        )
+    }
+
+    fn clear(&mut self) {
+        self.state.reset();
+    }
+}
+
+impl<B: Backend> Numeric for KLLossMetric<B> {
+    fn value(&self) -> f64 {
+        self.state.value()
+    }
+}
+
+pub struct NvidiaUtilMetric {
+    state: NumericMetricState,
+}
+
+impl NvidiaUtilMetric {
+    pub fn new() -> Self {
+        Self {
+            state: NumericMetricState::new(),
+        }
+    }
+}
+
+impl Default for NvidiaUtilMetric {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Metric for NvidiaUtilMetric {
+    const NAME: &'static str = "GPU Utilization";
+    type Input = ();
+
+    fn update(
+        &mut self,
+        _item: &(),
+        _metadata: &MetricMetadata,
+    ) -> MetricEntry {
+        let util = std::process::Command::new("nvidia-smi")
+            .arg("--query-gpu=utilization.gpu")
+            .arg("--format=csv,noheader,nounits")
+            .output()
+            .expect("failed to execute process")
+            .stdout;
+        let util = String::from_utf8(util).unwrap();
+        let util = util.trim().parse::<f64>().unwrap();
+
+        self.state.update(
+            util,
+            1,
+            FormatOptions::new(Self::NAME).precision(2),
+        )
+    }
+
+    fn clear(&mut self) {
+        self.state.reset();
+    }
+}
+
+impl Numeric for NvidiaUtilMetric {
+    fn value(&self) -> f64 {
+        self.state.value()
+    }
+}
